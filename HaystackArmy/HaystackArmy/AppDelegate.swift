3aeb91f1aad16fdd13d6c3ebccaa93d9746faa20
RajeshGandru <Rajeshgandru.rg@gmail.com>
2022-12-23 17:31:30 +0530

    project setup done

--- HaystackArmy/HaystackArmy/AppDelegate.swift
+++ HaystackArmy/HaystackArmy/AppDelegate.swift
@@ -1,37 +1,90 @@
 //
 //  AppDelegate.swift
-//  HaystackArmy
+//  HayStack-Army
 //
-//  Created by rajesh gandru on 23/12/22.
+//  Created by rajesh gandru on 12/05/21.
 //
 
 import UIKit
 import CoreData
+import GooglePlaces
+import IQKeyboardManagerSwift
+import GoogleMaps
+import FirebaseCore
+import Firebase
+import FirebaseMessaging
+ 
+
+var LognedUserType = ""
+var newDeviceId = ""
+var inServerSavedDeviceId = ""
+let GOOGLE_API_KEY = "AIzaSyAK7N4kOTSAWpSlzoOQk9_dKp9Sci2sshY"
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,MessagingDelegate{
 
-
-
+    var window: UIWindow?
+    let gcmMessageIDKey = "gcm.message_id"
+    var bgTask: UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier(rawValue: 0);
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        AppUpdater.shared.showUpdate(withConfirmation: false)
+        //API key in GMSServices and GMSPlaceClient
+        GMSServices.provideAPIKey(GOOGLE_API_KEY)
+        GMSPlacesClient.provideAPIKey(GOOGLE_API_KEY)
+        //FirebaseApp.configure()
+        self.FireBaseAppDelegateDidFineshMethod(application : application, launchOptions: launchOptions)
+        self.navigateToPage()
+        self.setupIQKeyboardManager()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    
+  
+    func navigateToPage(){
+        
+        if let loginedUser = UserDefaults.standard.string(forKey: "LoginedUserType")  as? String{
+            LognedUserType = loginedUser
+        }
+ 
+        if UserDefaults.standard.bool(forKey: "IsUserLogined") {
+          var navigation = UINavigationController()
+          let mainStoryboardIpad : UIStoryboard = UIStoryboard(name: "DashBoard", bundle: nil)
+        
+              let initialViewControlleripad : UIViewController = mainStoryboardIpad.instantiateViewController(withIdentifier: "mainTabvC") as! mainTabvC
+              self.window = UIWindow(frame: UIScreen.main.bounds)
+              navigation = UINavigationController(rootViewController: initialViewControlleripad)
+        
+          self.window?.rootViewController = navigation
+          self.window?.makeKeyAndVisible()
+        }else {
+            var navigation = UINavigationController()
+            let mainStoryboardIpad : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+          
+                let initialViewControlleripad : UIViewController = mainStoryboardIpad.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
+                self.window = UIWindow(frame: UIScreen.main.bounds)
+                navigation = UINavigationController(rootViewController: initialViewControlleripad)
+          
+            self.window?.rootViewController = navigation
+            self.window?.makeKeyAndVisible()
+        }
+          
+      }
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func setupIQKeyboardManager(){
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.resignFirstResponder()
+        IQKeyboardManager.shared.enableAutoToolbar = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
+     
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        AppUpdater.shared.showUpdate(withConfirmation: false)
     }
-
+    
+    
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
@@ -41,7 +94,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "HaystackArmy")
+        let container = NSPersistentContainer(name: "HayStack_Army")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
@@ -79,3 +132,243 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+
+extension AppDelegate {
+    
+    func FireBaseAppDelegateDidFineshMethod(application : UIApplication,launchOptions : [UIApplication.LaunchOptionsKey: Any]?){
+        
+        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        
+        
+        handleNotificationWhenAppIsKilled(launchOptions)
+         //remote Notifications
+        if #available(iOS 10.0, *) {
+             UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (isGranted, err) in
+                if err != nil {
+                    //Something bad happend
+                } else {
+                    UNUserNotificationCenter.current().delegate = self
+                    //  Messaging.messaging().delegate = self
+                    
+                    DispatchQueue.main.async {
+                        UIApplication.shared.registerForRemoteNotifications()
+                    }
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+         if #available(iOS 10, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.badge,.sound,.alert], completionHandler: { (granted, error) in
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            })
+        }else{
+            let notificationSettings = UIUserNotificationSettings(types: [.badge,.sound,.alert], categories: nil)
+            DispatchQueue.main.async {
+                UIApplication.shared.registerUserNotificationSettings(notificationSettings)
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    
+    
+    func handleNotificationWhenAppIsKilled(_ launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
+        
+        // Check if launched from the remote notification and application is close
+        
+        if let remoteNotification = launchOptions?[.remoteNotification] as?  [AnyHashable : Any] {
+            
+            let result = remoteNotification as! Dictionary<String,AnyObject>
+            
+            print(result)
+            
+            if let alert = result["type"] as? String {
+                
+            
+        }
+        }
+    }
+    
+}
+extension AppDelegate:UNUserNotificationCenterDelegate{
+    // Push Notification
+    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+        print("\n\n\n\n\n ==== FCM Token:  ",fcmToken)
+        connectToFcm()
+    }
+    
+   
+    
+    // Push Notification
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        // Print full message.
+        print(userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        print("tap on on forground app",userInfo)
+        
+        let result = userInfo as! Dictionary<String,AnyObject>
+        
+        print(result)
+        
+        let state = UIApplication.shared.applicationState
+        
+        if state == .background{
+            
+ 
+        }else if state == .active {
+            
+ 
+        }
+        
+        completionHandler(UIBackgroundFetchResult.newData)
+        
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+    }
+    
+    
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+        print(error.localizedDescription)
+        print("Not registered notification")
+    }
+    
+    
+    
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+        if let refreshedToken = Messaging.messaging().fcmToken {
+            print("InstanceID token: \(refreshedToken)")
+            newDeviceId = refreshedToken
+            print(newDeviceId)
+            Messaging.messaging().apnsToken = deviceToken
+            print("Token generated: ", refreshedToken)
+        }
+        UserDefaults.standard.set(newDeviceId, forKey:"device_id")
+        UserDefaults.standard.synchronize()
+        //        connectToFcm()
+    }
+    
+    func connectToFcm() {
+        Messaging.messaging().isAutoInitEnabled = true
+      //  Messaging.messaging().shouldEstablishDirectChannel = false
+
+        //messaging().shouldEstablishDirectChannel = true
+        if let token = Messaging.messaging().fcmToken {
+            print("\n\n\n\n\n\n\n\n\n\n ====== TOKEN DCS: " + token)
+        }
+    }
+    
+//    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+//
+//    }
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+//      let dataDict:[String: String] = ["token": fcmToken ?? ""]
+//      NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+        
+        
+        print("Firebase registration token: \(fcmToken)")
+        if let fcmTokenstr = fcmToken {
+        newDeviceId = fcmTokenstr
+        UserDefaults.standard.set(newDeviceId, forKey:"device_id")
+        UserDefaults.standard.synchronize()
+        let devicetoken : String!
+        devicetoken = UserDefaults.standard.string(forKey:"device_id") as String?
+        print(devicetoken)
+        }
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    
+    
+//    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+//        print("Received data message: \(remoteMessage.appData)")
+//    }
+    
+    @available(iOS 10.0, *)
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        let content = notification.request.content
+        
+        print("\(content.userInfo)")
+        
+        
+        
+        print("GOT A NOTIFICATION")
+        
+        
+        let result = content.userInfo as! Dictionary<String,AnyObject>
+        
+        print(result)
+        
+        let state = UIApplication.shared.applicationState
+        
+        if state == .background{
+            
+          
+            
+        }else if state == .active {
+            
+        
+        }
+        
+        
+//        if self.window?.rootViewController?.topViewController is ConversationVC {
+//            completionHandler([])
+//        } else {
+            completionHandler([.alert, .badge, .sound])
+       // }
+        
+        //completionHandler([.alert, .sound])
+        
+    }
+    
+    func saveChatIDS(ChatID : String){
+        let chatdata = UserDefaults.standard.value(forKey: "ChatIDArray") as? [String]
+        print(chatdata)
+        if chatdata?.count ?? 0 <= 0{
+            var dataArray = [String]()
+            dataArray.append(ChatID)
+            UserDefaults.standard.setValue(dataArray, forKey: "ChatIDArray")
+        }else{
+            print(chatdata)
+            var chatlistIds = [String]()
+            chatlistIds = chatdata ?? []
+            chatlistIds.append(ChatID)
+            UserDefaults.standard.setValue(chatlistIds, forKey: "ChatIDArray")
+        }
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "IGotChatMSG"), object: nil,userInfo: ["ChatID" : ChatID])
+    }
+  
+  
+    
+    
+    func alerfunc(msg : String){
+        let alert = UIAlertController(title: "HayStack -63d RD", message:msg, preferredStyle: UIAlertController.Style.alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        // show the alert
+        self.window?.rootViewController?.present(alert, animated: true, completion: nil)
+    }
+}
